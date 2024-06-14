a8c2f5b5124a3ed07c51b5d1a44d5ea4421f2405
tenaciosha <shagunchaudhary541@gmail.com>
2024-06-15 00:18:10 +0530

    Create datatypes.cpp

---
+++ basics-day1/datatypes.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a=10;
+    cout<< a <<endl;
+    char ch='a';
+    cout<< ch <<endl;
+    double d=1.23;
+    cout<< d <<endl;
+    float f=1.78;
+    cout<< f <<endl;
+    bool b=true;
+    cout<< b <<endl;
+    int size=sizeof(a);
+    cout<< "size of int is "<< size <<endl;
+}
